82425a9780db70fc210c8570cf2ad52996756a79
Susam Pal <susam@susam.in>
2013-12-01 15:35:14 +0530

    Remove lib/mathb directory from include path
    The lib directory contains the mathb package. The lib/mathb
    directory contains classes. For autoloading classes using
    spl_autoload, the include path should contain the path to the
    directory that contains the packages (directories corresponding
    to namespaces), not the directory that contains the classes.
    
    Discussion about why lib/mathb was added to the include path and
    why it is being removed is present at
    <https://github.com/susam/mathb/pull/1>.

--- mathbin.php
+++ mathbin.php
@@ -44,7 +44,6 @@
 require __DIR__ . '/thirdparty/php-markdown/Michelf/Markdown.php';
 
 set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/lib');
-set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/lib/mathb');
 spl_autoload_register();
 
 use Susam\Session;
